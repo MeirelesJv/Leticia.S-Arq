d9334add686f1e474a87eef81b7f8fec609b63f7
MeirelesJv <meirelesjv@hotmail.com>
2024-07-20 10:05:50 -0300

    commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express()
+const bodyParser = require("body-parser");
+
+app.use(express.static('style'));
+
+//Ejs
+app.set('view engine','ejs');
+
+//Body-Parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//Express
+app.listen(8080,()=>{console.log("Rodando!");});
