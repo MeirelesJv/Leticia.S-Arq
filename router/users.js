da04c7ec61179e7a8fa098248a35830bfb4187d2
MeirelesJv <meirelesjv@hotmail.com>
2024-07-23 23:09:07 -0300

    commit

--- router/users.js
+++ router/users.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const users = require("../database/users")
-const bcrypt = require("bcryptjs")
+const users = require("../database/users");
+const bcrypt = require("bcryptjs");
+const passwordRecover = require("../database/passwordReset");
+const envioEmail = require("./Email/email");
+const {  v4 : uuidv4  } = require("uuid")
 
 router.get("/login",(req,res)=>{
     res.render("users/login")
@@ -9,12 +12,24 @@ router.get("/login",(req,res)=>{
 
 router.get("/register", (req,res)=>{
     res.render("users/register")
-})
+});
 
+router.get("/recover/email", (req,res)=>{
+    res.render("users/recoverEmail")
+});
+
+router.get("/login/recover",(req,res)=>{
+    res.render("users/recover")
+});
+
+router.get("/logout",(req,res) =>{
+    req.session.user = undefined;
+    res.redirect("/");
+});
 
 router.post("/users/register", async (req,res)=>{
     let {name,surname,email,cpf,birth,password,tel,cep,road,neighborhood,uf,city} = req.body
-    const type = "1"
+    const type = "0"
 
     try {
         //Validar email
@@ -54,7 +69,7 @@ router.post("/users/register", async (req,res)=>{
         console.error(error);
         res.status(500).json({ error: 'Registration failed' });
     }
-})
+});
 
 router.post("/users/login", async (req,res)=>{
     let {email,password} = req.body
@@ -77,11 +92,65 @@ router.post("/users/login", async (req,res)=>{
         }
     })
 
-})
+});
 
-router.get("/logout",(req,res) =>{
-    req.session.user = undefined;
-    res.redirect("/");
-})
+router.post("/users/recover/email", async (req,res)=>{
+    let {email} = req.body
+    var token = uuidv4()
+    
+    let existingEmail = await users.findOne({ where: {Email: email}});
+    if(existingEmail != undefined){
+        
+        await passwordRecover.create({
+            Token: token,
+            Email: email,
+            Use: '0'
+        })
+
+        await envioEmail.sendMail({
+            from: "Joao Vitor <meirelesDev@hotmail.com>",
+            to: email,
+            subject: "Recuperação de Senha!",
+            text: "Token de recuperação de senha: <br>" + token,
+        });
+
+        res.redirect('/login/recover')
+
+    }else{
+        return res.status(400).json({message: "Email não encontrado"})
+    }
+});
+
+router.post("/users/recover", async (req,res)=>{
+    let {password,token} = req.body
+
+    const tokenValid = await passwordRecover.findOne({where: {Token: token}})
+    if(tokenValid != undefined){
+        var email = tokenValid.dataValues.Email
+        var tokenUser = tokenValid.dataValues.Token
+        if(tokenValid.dataValues.Use === '0'){
+            const salt = bcrypt.genSaltSync(10);
+            const hash = bcrypt.hashSync(password, salt);
+
+            await users.update({Password: hash},{
+                where: {
+                    Email: email
+                }
+            })
+            await passwordRecover.update({Use: '1'},{
+                where: {
+                    Token: tokenUser
+                }
+            })
+
+            res.redirect("/login")
+            return res.status(200)
+        }else{
+            return res.status(400).json({message: "Token Invalido"})
+        }
+    }else{
+        return res.status(400).json({message: "Token Invalido"})
+    }
+});
 
 module.exports = router;
