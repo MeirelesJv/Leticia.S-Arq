b87b1ed7f7f09b3abd2485f10edfc14cc560108f
meirelesjv <meirelesjv@hotmail.com>
2024-08-19 20:03:03 -0300

    commit

--- Keys.js
+++ Keys.js
@@ -1,9 +1,9 @@
 var keys = {
-    databasePassword: '',
-    JWTSecret: '',
-    Email: '',
-    emailPassword: '',
-    sessionPassword: ''
+    databasePassword: 'J0@0V1T0Rjvv',
+    JWTSecret: 'bamlrçdkgmnaerçnoht',
+    Email: 'meirelesdev@hotmail.com',
+    emailPassword: 'j0@0v1t0r',
+    sessionPassword: 'imrpivarv4094cl'
 }
 
 module.exports = keys;
